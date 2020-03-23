d47f8ed6c7896a3b458b609ba0431df2048ec010
lqn <1813933430@qq.com>
2020-03-23 18:43:31 +0800

    基本资料

---
+++ FristGroupHIS/src/tool/DBUtil.java
@@ -0,0 +1,30 @@
+package tool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DBUtil {
+	private static ComboPooledDataSource source = new ComboPooledDataSource("mysql-config");
+	public static Connection getSqlConnection(){
+		Connection con = null;
+		try {
+			con = source.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	public static void closeSqlSource(AutoCloseable...close){
+		for (AutoCloseable autoCloseable : close) {
+			try {
+				autoCloseable.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
